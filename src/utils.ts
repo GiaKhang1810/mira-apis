2e546165baf5a444fd6b9b93ca1e023d2ac5eb40
GiaKhang1810 <ngkhang9a5lqc11@gmail.com>
2025-03-19 12:00:12 +0700

    Enhanced output data type management

--- src/utils.ts
+++ src/utils.ts
@@ -4,18 +4,14 @@ import chalk from "chalk";
 
 const ERROR_LOG = path.resolve(process.env.ERROR_LOG || "./database/error.log");
 
-export function getTime(format: string = "HH:mm:ss DD/MM/YYYY", date: Date = new Date()): string {
-    format = format ? format : "HH:mm:ss DD/MM/YYYY";
-    const cDate = date ? date : new Date();
-
-
-    const HH = String(cDate.getHours()).padStart(2, "0");
-    const mm = String(cDate.getMinutes()).padStart(2, "0");
-    const ss = String(cDate.getSeconds()).padStart(2, "0");
-    const ms = String(cDate.getMilliseconds()).padStart(3, "0");
-    const DD = String(cDate.getDate()).padStart(2, "0");
-    const MM = String(cDate.getMonth() + 1).padStart(2, "0");
-    const YYYY = String(cDate.getFullYear());
+export function getTime(format: string = "HH:mm:ss DD/MM/YYYY", cDate: Date = new Date()): string {
+    const HH: string = String(cDate.getHours()).padStart(2, "0");
+    const mm: string = String(cDate.getMinutes()).padStart(2, "0");
+    const ss: string = String(cDate.getSeconds()).padStart(2, "0");
+    const ms: string = String(cDate.getMilliseconds()).padStart(3, "0");
+    const DD: string = String(cDate.getDate()).padStart(2, "0");
+    const MM: string = String(cDate.getMonth() + 1).padStart(2, "0");
+    const YYYY: string = cDate.getFullYear().toString();
 
     return format
         .replace(/\HH|MM|ss|mm|DD|YYYY|ms/g, (key: string): string => {
@@ -42,15 +38,15 @@ export function getTime(format: string = "HH:mm:ss DD/MM/YYYY", date: Date = new
 
 export const log = {
     info: (name: string, message: string): void => {
-        const time = getTime("[ HH:mm:ss | DD/MM/YYYY ]");
+        const time: string = getTime("[ HH:mm:ss | DD/MM/YYYY ]");
         console.log(chalk.green(time), name + ":", message);
     },
     warn: (name: string, message: string): void => {
-        const time = getTime("[ HH:mm:ss | DD/MM/YYYY ]");
+        const time: string = getTime("[ HH:mm:ss | DD/MM/YYYY ]");
         console.log(chalk.yellow(time), name + ":", message);
     },
     error: (name: string, error: Error): void => {
-        const time = getTime("[ HH:mm:ss | DD/MM/YYYY ]");
+        const time: string = getTime("[ HH:mm:ss | DD/MM/YYYY ]");
         console.log(chalk.red(time), name + ":", error.message);
         const ErrorLog =
             time +
@@ -66,11 +62,7 @@ export const log = {
             fs.appendFileSync(ERROR_LOG, ErrorLog);
         }
     },
-    wall: (len: number | null | undefined): void => {
-        len = len ? len : 15;
-
-        console.log(chalk.blue("=".repeat(len)));
-    }
+    wall: (len: number = 15): void => console.log(chalk.blue("=".repeat(len)))
 }
 
 export const getType = (data: unknown): string => Object.prototype.toString.call(data).slice(8, -1);
